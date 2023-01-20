ce782deb06d8ea6813df2d72b5c82f3c2d0f9813
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-01-20 18:20:20 -0500

    created template

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d')
+
+const background = new Image()
+background.src = './images/background.gif'
+
+const ship = new Image()
+ship.src = './images/spaceship.png'
+
+const aliens = new Image()
+aliens.src = './images/sci-fi.png'
+
+let intervalId;
+let animationId;
+
+function animationLoop() {
+    animationId = setInterval(()=>{
+      updateCanvas()
+    }, 16)
+  }
+
+  function startGame() {
+  
+    player.x = startingX
+    player.y = startingY
+  
+    ctx.drawImage(road, 0, 0, 500, 700)
+    player.draw()
+    createObstacle()
+    animationLoop()
+  
+  }
